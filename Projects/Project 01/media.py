26f738a9e0e31bb32bf448475a83d4d5825b26b6
Thomas Mühlegger <muehleggerth@stud.sbg.ac.at>
2016-04-11 16:59:44 +0200

    Add some comments

--- Projects/Project 01/media.py
+++ Projects/Project 01/media.py
@@ -1,5 +1,13 @@
 class Movie():
-    """Movie class containing some informations about a movie"""
+    """Movie class containing some informations about a movie
+    
+    Attributes:
+        title: Title of the movie
+        description: Description of the movie
+        poster_image_url: URL of the poster image
+        trailer_youtube_url: URL of the youtube trailer
+    """
+    
     def __init__(self, movie_title, movie_description, poster_image_url, trailer_youtube_url):
         """Inits a Movie with specified informations."""
         self.title = movie_title
