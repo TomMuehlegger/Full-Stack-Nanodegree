08d9b61597050cf74ec699c6710f314d82d0b6cd
Thomas Mühlegger <muehleggerth@stud.sbg.ac.at>
2016-04-26 22:56:47 +0200

    Review
    Did the review suggestions and requirements

--- Project 03/application.py
+++ Project 03/application.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request, redirect, jsonify, url_for
+from functools import wraps
 from sqlalchemy import create_engine, desc
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 from database_setup import Base, User, Item, Category
 from flask import session as login_session
 import random
 import string
 import httplib2
 import json
-from flask import make_response, flash
+from flask import make_response, flash, abort
 
 app = Flask(__name__)
 
@@ -18,6 +20,17 @@ Base.metadata.bind = engine
 # Make the database session
 DBSession = sessionmaker(bind=engine)
 db_session = DBSession()
+
+
+def login_required(f):
+    """ Login required decorator """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if authenticated() == 0:
+            abort(401)
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 @app.route('/login')
@@ -51,7 +64,7 @@ def fbconnect():
                             .read())['web']['app_secret']
 
     # URL to check the access_token
-    url = 'https://graph.facebook.com/oauth/access_token?' \
+    url = 'https://graph.facebook.com/oauth/access_token?'\
           'grant_type=fb_exchange_token&client_id=%s&client_secret=%s&'\
           'fb_exchange_token=%s' % (app_id, app_secret, access_token)
 
@@ -168,10 +181,13 @@ def editable(itemOrCategory):
 @app.route('/catalog/<int:category_id>/JSON')
 def catalogJSON(category_id):
     """ Returns all categories with items in JSON format """
-    categories = db_session.query(Category).filter_by(id=category_id).one()
-    items = db_session.query(Item).filter_by(category_id=category_id).all()
-    return jsonify(Category=categories.serialize,
-                   Items=[i.serialize for i in items])
+    try:
+        categories = db_session.query(Category).filter_by(id=category_id).one()
+        items = db_session.query(Item).filter_by(category_id=category_id).all()
+        return jsonify(Category=categories.serialize,
+                       Items=[i.serialize for i in items])
+    except NoResultFound:
+        return '[404]'
 
 
 @app.route('/')
@@ -207,18 +223,13 @@ def showItem(item_id):
 
 
 @app.route('/catalog/category/new', methods=['GET', 'POST'])
+@login_required
 def newCategory():
     """ Creates a new category """
-    user_id = authenticated()
-
-    if user_id == 0:
-        # If no user is logged in -> redirect to login page
-        return redirect(url_for('login'))
-
     if request.method == 'POST':
         # Create the new category object
         newCategory = Category(name=request.form['name'],
-                               creator_id=user_id)
+                               creator_id=login_session['user_id'])
         db_session.add(newCategory)
         db_session.commit()
         # Create flash message
@@ -229,21 +240,16 @@ def newCategory():
 
 
 @app.route('/catalog/item/new', methods=['GET', 'POST'])
+@login_required
 def newItem():
     """ Creates a new item """
-    user_id = authenticated()
-
-    if user_id == 0:
-        # If no user is logged in -> redirect to login page
-        return redirect(url_for('login'))
-
     if request.method == 'POST':
         # Create the new item object
         newItem = Item(title=request.form['title'],
                        description=request.form['description'],
                        price=request.form['price'],
                        category_id=request.form['category_id'],
-                       creator_id=user_id)
+                       creator_id=login_session['user_id'])
         db_session.add(newItem)
         db_session.commit()
         # Create flash message
@@ -255,6 +261,7 @@ def newItem():
 
 
 @app.route('/catalog/category/<int:category_id>/edit', methods=['GET', 'POST'])
+@login_required
 def editCategory(category_id):
     """ Edits an existing category """
     editCategory = db_session.query(Category).filter_by(id=category_id).one()
@@ -273,6 +280,7 @@ def editCategory(category_id):
 
 
 @app.route('/catalog/item/<int:item_id>/edit', methods=['GET', 'POST'])
+@login_required
 def editItem(item_id):
     """ Edits an existing item """
     editItem = db_session.query(Item).filter_by(id=item_id).one()
@@ -300,6 +308,7 @@ def editItem(item_id):
 
 @app.route('/catalog/category/<int:category_id>/delete',
            methods=['GET', 'POST'])
+@login_required
 def deleteCategory(category_id):
     """ Deletes an existing category """
     deleteCategory = db_session.query(Category).filter_by(id=category_id).one()
@@ -317,6 +326,7 @@ def deleteCategory(category_id):
 
 
 @app.route('/catalog/item/<int:item_id>/delete', methods=['GET', 'POST'])
+@login_required
 def deleteItem(item_id):
     """ Deletes an existing item """
     deleteItem = db_session.query(Item).filter_by(id=item_id).one()
@@ -334,5 +344,5 @@ def deleteItem(item_id):
 
 if __name__ == '__main__':
     app.secret_key = 'my_secret_key'
-    app.debug = True
+    app.debug = False
     app.run(host='0.0.0.0', port=8000)
